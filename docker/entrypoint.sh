61c3e4db18aa8767bd39cc81eaa19466d4c57799
Tan <jimerson.tan@urios.edu.ph>
2026-04-28 23:39:36 +0800

    Fix: Add automatic migrations to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,8 +3,8 @@
 # Start PHP-FPM
 php-fpm -D
 
-# Run migrations (Optional: use --force for production)
-# php artisan migrate --force
+# Run migrations
+php artisan migrate --force
 
 # Start Nginx
 nginx -g "daemon off;"

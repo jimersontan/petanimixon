95cff1a9ea2bf94de586f4c3330458c57cfc32da
Tan <jimerson.tan@urios.edu.ph>
2026-04-29 00:07:34 +0800

    Fix: Make pgsql sslmode dynamic for Render

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
         'sqlsrv' => [

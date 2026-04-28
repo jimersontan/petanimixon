f577781403227627f9db78faa4201f240e18281f
Tan <jimerson.tan@urios.edu.ph>
2026-04-28 23:49:46 +0800

    Fix: Add permission setup to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,10 @@
 # Start PHP-FPM
 php-fpm -D
 
+# Fix permissions
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
 # Run migrations
 php artisan migrate --force
 
